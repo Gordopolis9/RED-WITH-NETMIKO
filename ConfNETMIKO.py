d88d85c373aa665f9b4c57412b4073ddcbf7c0c9
Gordopolis9 <valentin.soveroniselle@gmail.com>
2025-08-13 00:34:17 -0300

    Cambio README.md y comienzo conf

--- ConfNETMIKO.py
+++ ConfNETMIKO.py
@@ -1,2 +1,21 @@
 from netmiko import ConnectHandler
 
+routers_mikrotik = [
+    {
+        # Router LOCAL
+        'device_type': 'mikrotik_routeros',
+        'host': 'COLOCAR IP',
+        'username' : 'admin',
+        'password' : 'admin',
+        'port' : 22,
+        'secret' : '',
+        'config' : {
+            'ip_address-VENTAS-280' : '10.10.17.1',
+            'interface-VENTAS-280' : 'VLAN_VENTAS',
+            'ip_addres-external' : '10.10.17.65',
+            'interface-external' : 'ether1',
+        }
+
+
+    }
+]
