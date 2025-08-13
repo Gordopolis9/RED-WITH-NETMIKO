6a9580f4251710750e70e7cb08d1684392bb4f92
Gordopolis9 <valentin.soveroniselle@gmail.com>
2025-08-13 11:53:26 -0300

    Preset ROUTER 1

--- ConfNETMIKO.py
+++ ConfNETMIKO.py
@@ -3,19 +3,28 @@ from netmiko import ConnectHandler
 routers_mikrotik = [
     {
         # Router LOCAL
+        'name' : 'local_route',
         'device_type': 'mikrotik_routeros',
         'host': 'COLOCAR IP',
         'username' : 'admin',
         'password' : 'admin',
         'port' : 22,
         'secret' : '',
-        'config' : {
-            'ip_address-VENTAS-280' : '10.10.17.1',
-            'interface-VENTAS-280' : 'VLAN_VENTAS',
-            'ip_addres-external' : '10.10.17.65',
-            'interface-external' : 'ether1',
+        'local_config' :{
+            'vlans' : [
+            {'VLAN_NAME' : 'VLAN_VENTAS', 'VLAN_ID' : 280, 'VLAN_GATEWAY' : "10.10.17.1/27" },
+            {'VLAN_NAME' : 'VLAN_TECNICA', 'VLAN_ID' : 281, 'VLAN_GATEWAY' : "10.10.17.33/28" },
+            {'VLAN_NAME' : 'VLAN_VISITANTES', 'VLAN_ID' : 282, 'VLAN_GATEWAY' : "10.10.17.49/29" },
+            {'VLAN_NAME' : 'VLAN_GESTION', 'VLAN_ID' : 1799, 'VLAN_GATEWAY' : "10.10.17.57/29" }
+            ],
+            'interface' : 'ether2',
+        },
+        'external_config' : {
+            'interface' : 'ether3',
+            'ip' : '10.10.17.65/30',
+            'next_hop' : '10.10.17.66'
         }
-
+        
 
     }
 ]
